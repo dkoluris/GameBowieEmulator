2e45fec29f6e28c7645a95aaffb546a1df604b47
Dennis Koluris <denniskoluris@rezolve.com>
2020-12-09 12:24:43 +0200

    Mem modules

--- src/mem.js
+++ src/mem.js
@@ -19,11 +19,11 @@ GameBowie.CstrMem = function() {
     function writeMBC1(addr, data) {
         let bank;
 
-        switch (addr) {
-            case 0x0000 ... 0x1fff:
+        switch (true) {
+            case (addr >= 0x0000 && addr <= 0x1fff):
                 return false;
 
-            case 0x2000 ... 0x3fff:
+            case (addr >= 0x2000 && addr <= 0x3fff):
                 bank = data & 0x1f;
 
                 if (!ramSelect) {
@@ -34,14 +34,14 @@ GameBowie.CstrMem = function() {
                     bank++;
                 }
 
-                memcp(&ram.ptr[0x4000], &rom.ptr[bank * 0x4000], 0x4000);
+                // TODO: memcp(&ram.ptr[0x4000], &rom.ptr[bank * 0x4000], 0x4000);
                 return false;
 
-            case 0x4000 ... 0x5fff:
+            case (addr >= 0x4000 && addr <= 0x5fff):
                 bankUpperBits = (data & 0x3) << 5;
                 return false;
 
-            case 0x6000 ... 0x7fff:
+            case (addr >= 0x6000 && addr <= 0x7fff):
                 ramSelect = data & 1;
                 return false;
         }
@@ -82,8 +82,8 @@ GameBowie.CstrMem = function() {
             }
 
             // Write at most 2 mem banks
-            memcp(&ram.ptr[0x0000], &rom.ptr[0x0000], 0x4000);
-            memcp(&ram.ptr[0x4000], &rom.ptr[0x4000], 0x4000);
+            // TODO: memcp(&ram.ptr[0x0000], &rom.ptr[0x0000], 0x4000);
+            // TODO: memcp(&ram.ptr[0x4000], &rom.ptr[0x4000], 0x4000);
         },
 
         write: {
@@ -108,54 +108,54 @@ GameBowie.CstrMem = function() {
                         break;
                 }
 
-                switch (addr) {
-                    case 0xe000 ... 0xfdff: // Prohibited: Echo RAM ???
+                switch (true) {
+                    case (addr >= 0xe000 && addr <= 0xfdff): // Prohibited: Echo RAM ???
                         mem.ram.ub[addr - 0x2000] = data;
                         return;
 
-                    case 0x8000 ... 0x9fff: // VRAM, cleared on boot
-                    case 0xa000 ... 0xbfff: // Cart RAM ???
-                    case 0xc000 ... 0xcfff: // Work RAM Bank 0
-                    case 0xd000 ... 0xdfff: // Work RAM Bank 1
-                    case 0xff80 ... 0xfffe: // HRAM stack
-                    case 0xfe00 ... 0xfe9f: // OAM Sprite
-                    case 0xfea0 ... 0xfeff: // Not usable
-                    case 0xff01: // Link port data ?
-                    case 0xff02: // ?
+                    case (addr >= 0x8000 && addr <= 0x9fff): // VRAM, cleared on boot
+                    case (addr >= 0xa000 && addr <= 0xbfff): // Cart RAM ???
+                    case (addr >= 0xc000 && addr <= 0xcfff): // Work RAM Bank 0
+                    case (addr >= 0xd000 && addr <= 0xdfff): // Work RAM Bank 1
+                    case (addr >= 0xff80 && addr <= 0xfffe): // HRAM stack
+                    case (addr >= 0xfe00 && addr <= 0xfe9f): // OAM Sprite
+                    case (addr >= 0xfea0 && addr <= 0xfeff): // Not usable
+                    case (addr == 0xff01): // Link port data ?
+                    case (addr == 0xff02): // ?
 
-                    case 0xff4c:
-                    case 0xff4d:
-                    case 0xff4e:
-                    case 0xff4f:
-                    case 0xff51 ... 0xff7f: // ?
+                    case (addr == 0xff4c):
+                    case (addr == 0xff4d):
+                    case (addr == 0xff4e):
+                    case (addr == 0xff4f):
+                    case (addr >= 0xff51 && addr <= 0xff7f): // ?
                         mem.ram.ub[addr] = data;
                         return;
 
-                    case 0xff00: // Gamepad
+                    case (addr == 0xff00): // Gamepad
                         input.write(data);
                         return;
 
-                    case 0xff04 ... 0xff07: // Timers
+                    case (addr >= 0xff04 && addr <= 0xff07): // Timers
                         timers.write(addr, data);
                         return;
 
-                    case 0xff10 ... 0xff3f: // Audio
+                    case (addr >= 0xff10 && addr <= 0xff3f): // Audio
                         audio.write(addr, data);
                         return;
 
-                    case 0xff40 ... 0xff4b: // LCD
+                    case (addr >= 0xff40 && addr <= 0xff4b): // LCD
                         screen.write(addr, data);
                         return;
 
-                    case 0xff50: // Boot ROM completed
+                    case (addr == 0xff50): // Boot ROM completed
                         bootstrap.enabled = false;
                         return;
 
-                    case 0xff0f: // Interrupt
+                    case (addr == 0xff0f): // Interrupt
                         bus.interrupts.data = data;
                         return;
 
-                    case 0xffff: // Interrupt Mask
+                    case (addr == 0xffff): // Interrupt Mask
                         bus.interrupts.mask = data;
                         return;
                 }
@@ -176,42 +176,42 @@ GameBowie.CstrMem = function() {
                     }
                 }
 
-                switch (addr) {
-                    case 0xe000 ... 0xfdff: // Prohibited: Echo RAM ???
+                switch (true) {
+                    case (addr >= 0xe000 && addr <= 0xfdff): // Prohibited: Echo RAM ???
                         return mem.ram.ub[addr - 0x2000];
 
-                    case 0x0000 ... 0x3fff: // ROM Bank 0
-                    case 0x4000 ... 0x7fff: // ROM Bank 1
-                    case 0x8000 ... 0x9fff: // VRAM
-                    case 0xa000 ... 0xbfff: // Cart RAM ???
-                    case 0xc000 ... 0xcfff: // Work RAM Bank 0
-                    case 0xd000 ... 0xdfff: // Work RAM Bank 1
-                    case 0xff80 ... 0xfffe: // HRAM stack
-                    case 0xfe00 ... 0xfe9f: // OAM Sprite
-                    case 0xff50 ... 0xff7f: // ?
-                    case 0xff01: // ?
-                    case 0xff02: // ?
+                    case (addr >= 0x0000 && addr <= 0x3fff): // ROM Bank 0
+                    case (addr >= 0x4000 && addr <= 0x7fff): // ROM Bank 1
+                    case (addr >= 0x8000 && addr <= 0x9fff): // VRAM
+                    case (addr >= 0xa000 && addr <= 0xbfff): // Cart RAM ???
+                    case (addr >= 0xc000 && addr <= 0xcfff): // Work RAM Bank 0
+                    case (addr >= 0xd000 && addr <= 0xdfff): // Work RAM Bank 1
+                    case (addr >= 0xff80 && addr <= 0xfffe): // HRAM stack
+                    case (addr >= 0xfe00 && addr <= 0xfe9f): // OAM Sprite
+                    case (addr >= 0xff50 && addr <= 0xff7f): // ?
+                    case (addr == 0xff01): // ?
+                    case (addr == 0xff02): // ?
                         return mem.ram.ub[addr];
 
-                    case 0xff00: // Gamepad
+                    case (addr == 0xff00): // Gamepad
                         return input.read();
 
-                    case 0xff04 ... 0xff07: // Timers
+                    case (addr >= 0xff04 && addr <= 0xff07): // Timers
                         return timers.read(addr);
 
-                    case 0xff10 ... 0xff3f: // Audio
+                    case (addr >= 0xff10 && addr <= 0xff3f): // Audio
                         return audio.read(addr);
 
-                    case 0xff40 ... 0xff4b: // LCD
+                    case (addr >= 0xff40 && addr <= 0xff4b): // LCD
                         return screen.read(addr);
 
-                    case 0xff4d: // GBC speed switch
+                    case (addr == 0xff4d): // GBC speed switch
                         return 0xff;
 
-                    case 0xff0f: // Interrupt
+                    case (addr == 0xff0f): // Interrupt
                         return bus.interrupts.data;
 
-                    case 0xffff: // Interrupt Mask
+                    case (addr == 0xffff): // Interrupt Mask
                         return bus.interrupts.mask;
                 }
 
